1c28bfc2a5981e253284c142135601ba87953f3f
ErickSeo <hiroyukiiseo@gmail.com>
2024-07-28 12:51:50 -0300

    test

--- airflow/scripts/users_by_city.py
+++ airflow/scripts/users_by_city.py
@@ -1,7 +1,21 @@
 import pyspark
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('users-by-city-job').getOrCreate()
+conf = SparkConf()
+conf.set("spark.yarn.maxAppAttempts", "1")
+conf.set("spark.hadoop.fs.s3a.endpoint", "minio-service.minio.svc.cluster.local:9000")
+conf.set("spark.hadoop.fs.s3a.access.key", "minioadmin")
+conf.set("spark.hadoop.fs.s3a.secret.key", "minioadmin")
+conf.set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
+# "Enable S3 path style access ie disabling the default virtual hosting behaviour.
+# Useful for S3A-compliant storage providers as it removes the need to set up DNS for virtual hosting."
+conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
+# Set Committer config compliant with MinIO
+conf.set("spark.hadoop.fs.s3a.committer.name", "directory")
+conf.set("spark.hadoop.fs.s3a.committer.staging.conflict-mode", "replace")
+conf.set("spark.hadoop.fs.s3a.committer.staging.tmp.path", "/tmp/staging")
+
+spark = SparkSession.builder.appName("Dockerized Minio Spark Test").config(conf=conf).getOrCreate()
 
 json = [
   {
@@ -114,3 +128,5 @@ spark.sql("""
   from usuarios
   group by city
 """).show(truncate=False)
+
+df_users.write.mode("overwrite").parquet(f"s3a://raw/file.parquet")
