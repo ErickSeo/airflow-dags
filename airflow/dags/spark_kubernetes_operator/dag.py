295ba7450f64911f67f891efbdf538f88d62b548
ErickSeo <hiroyukiiseo@gmail.com>
2024-07-27 22:23:40 -0300

    test

--- airflow/dags/spark_kubernetes_operator/dag.py
+++ airflow/dags/spark_kubernetes_operator/dag.py
@@ -1,88 +1,38 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-This is an example DAG which uses SparkKubernetesOperator and SparkKubernetesSensor.
-In this example, we create two tasks which execute sequentially.
-The first task is to submit sparkApplication on Kubernetes cluster(the example uses spark-pi application).
-and the second task is to check the final state of the sparkApplication that submitted in the first state.
-
-Spark-on-k8s operator is required to be already installed on Kubernetes
-https://github.com/GoogleCloudPlatform/spark-on-k8s-operator
-"""
-
-from datetime import timedelta, datetime
-
-# [START import_module]
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-# Operators; we need this to operate!
+from datetime import timedelta, datetime
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
-from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from kubernetes.client import models as k8s
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-# [END import_module]
-
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 3
+default_args={
+   'depends_on_past': False,
+   'email': ['abcd@gmail.com'],
+   'email_on_failure': False,
+   'email_on_retry': False,
+   'retries': 1,
+   'retry_delay': timedelta(minutes=5)
 }
-# [END default_args]
-
-# [START instantiate_dag]
-
-dag = DAG(
-    'spark_pi',
-    default_args=default_args,
-    schedule_interval=timedelta(days=1),
-    tags=['example']
-)
-
-# spark = open(
-#     "example_spark_kubernetes_operator_pi.yaml").read()
-
-submit = SparkKubernetesOperator(
-    task_id='spark_pi_submit',
-    namespace="sampletenant",
-    application_file="example_spark_kubernetes_operator_pi.yaml",
-    kubernetes_conn_id="kubernetes_in_cluster",
-    do_xcom_push=True,
-    dag=dag,
-    api_group="sparkoperator.hpe.com",
-    enable_impersonation_from_ldap_user=False
-)
-
-sensor = SparkKubernetesSensor(
-    task_id='spark_pi_monitor',
-    namespace="sampletenant",
-    application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-    kubernetes_conn_id="kubernetes_in_cluster",
-    dag=dag,
-    api_group="sparkoperator.hpe.com",
-    attach_log=True
-)
-
-submit >> sensor
+with DAG(
+   'my-second-dag',
+   default_args=default_args,
+   description='simple dag',
+   schedule_interval=timedelta(days=1),
+   start_date=datetime(2022, 11, 17),
+   catchup=False,
+   tags=['example']
+) as dag:
+   t1 = SparkKubernetesOperator(
+       task_id='n-spark-pi',
+       trigger_rule="all_success",
+       depends_on_past=False,
+       retries=3,
+       application_file="new-spark-pi.yaml",
+       namespace="spark-jobs",
+       kubernetes_conn_id="myk8s",
+       api_group="sparkoperator.k8s.io",
+       api_version="v1beta2",
+       do_xcom_push=True,
+       dag=dag
+   )
