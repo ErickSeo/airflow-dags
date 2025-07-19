9bda0cd19e68c3824f1af4b563960e1e8f19498b
ErickSeo <hiroyukiiseo@gmail.com>
2025-07-19 16:53:47 -0300

    teste

---
+++ airflow/dags/teste/dag_factory.py
@@ -0,0 +1,17 @@
+#colocar hash do commit aqui para forçar
+import os
+from pathlib import Path
+
+# The following import is here so Airflow parses this file
+# from airflow import DAG
+
+import dagfactory
+
+
+config_file = Path.cwd() / "dags/repo/airflow/dags/teste/config.yaml"
+
+example_dag_factory = dagfactory.DagFactory(config_file)
+
+# Creating task dependencies
+example_dag_factory.clean_dags(globals())
+example_dag_factory.generate_dags(globals())
--- airflow/dags/teste2/teste_dag_factory.py
+++ airflow/dags/teste2/teste_dag_factory.py
@@ -1,17 +1,17 @@
-#colocar hash do commit aqui para forçar
-import os
-from pathlib import Path
+# #colocar hash do commit aqui para forçar
+# import os
+# from pathlib import Path
 
-# The following import is here so Airflow parses this file
-# from airflow import DAG
+# # The following import is here so Airflow parses this file
+# # from airflow import DAG
 
-import dagfactory
+# import dagfactory
 
 
-config_file = Path.cwd() / "dags/repo/airflow/dags/teste2/config.yaml"
+# config_file = Path.cwd() / "dags/repo/airflow/dags/teste2/config.yaml"
 
-example_dag_factory = dagfactory.DagFactory(config_file)
+# example_dag_factory = dagfactory.DagFactory(config_file)
 
-# Creating task dependencies
-example_dag_factory.clean_dags(globals())
-example_dag_factory.generate_dags(globals())
+# # Creating task dependencies
+# example_dag_factory.clean_dags(globals())
+# example_dag_factory.generate_dags(globals())
