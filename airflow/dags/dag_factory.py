2e2ceeb7e1061d6d08a5956cafe707e12ab0ee9e
ErickSeo <hiroyukiiseo@gmail.com>
2025-07-21 17:45:48 -0300

    retsefsdafsa

--- airflow/dags/dag_factory.py
+++ airflow/dags/dag_factory.py
@@ -1,9 +1,8 @@
-#dhsaoidhiosa
 from pathlib import Path
 from dagfactory import load_yaml_dags
 
 
-config_file = Path.cwd() / "dags/repo/airflow/dags/"
+config_file = Path.cwd() / "dags/repo/airflow/dags"
 
 load_yaml_dags(
     globals_dict=globals(),
